b825f88e16b10f1e95bc9de0911f2009f92fd436
VasiliyF <vasiliy.fofanov@quest.com>
2020-11-15 10:39:08 +0300

    Implement upload to blob container

--- FunctionApp/Functions/CopyJsonFromPublicApiFunction.cs
+++ FunctionApp/Functions/CopyJsonFromPublicApiFunction.cs
@@ -1,10 +1,12 @@
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace DeviconTestFunctionApp
 {
@@ -13,18 +15,50 @@ namespace DeviconTestFunctionApp
         [FunctionName("CopyJsonFromPublicApiFunction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "copy-public-api")]
-            HttpRequest req, ILogger log)
+            HttpRequest req
+            , ExecutionContext executionContext, ILogger log)
         {
             log.LogInformation("Copy public API function processed a request.");
+
+            const string blobContainerName = "public-api-files";
+            var storageAccount = GetCloudStorageAccount(executionContext);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            if (await CreateContainerIfNotExistsTaskAsync(blobClient, blobContainerName))
+            {
+                log.LogInformation($"Blob container '{blobContainerName}' created");
+            }
+            var container = blobClient.GetContainerReference(blobContainerName);
+
 
             return await PublicApiHelper.ReadData(req, async (stream, name) =>
             {
                 await using (stream)
                 {
-                    using var reader = new StreamReader(stream);
-                    return new OkObjectResult(await reader.ReadToEndAsync());
+                    var blobName = $"{name}.json";
+                    var blob = container.GetBlockBlobReference(blobName);
+                    blob.Properties.ContentType = "application/json";
+                    await blob.UploadFromStreamAsync(stream);
+
+                    return new OkObjectResult($"Blob '{blobName}' uploaded to container '{blobContainerName}' successfully");
                 }
             });
+        }
+
+        private static CloudStorageAccount GetCloudStorageAccount(ExecutionContext executionContext)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(executionContext.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", true, true)
+                .AddEnvironmentVariables().Build();
+
+            return CloudStorageAccount.Parse(config["AzureWebJobsStorage"]);
+        }
+
+        private static Task<bool> CreateContainerIfNotExistsTaskAsync(CloudBlobClient blobClient, string blobContainerName)
+        {
+            var blobContainer = blobClient.GetContainerReference(blobContainerName);
+            return blobContainer.CreateIfNotExistsAsync();
         }
     }
 }
